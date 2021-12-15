4f21553c9218170c15049b8a9fd3478c90cba377
Emil Folino <emilfolino@gmail.com>
2021-12-15 15:03:27 +0100

    Add routes to flask app

--- flask_starter/app.py
+++ flask_starter/app.py
@@ -1,7 +1,24 @@
 from flask import Flask
+from markupsafe import escape
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
     return "Hello World!"
+
+@app.route("/another")
+def another():
+    return "Another Hello World!"
+
+@app.route("/dynamic/<name>")
+def dynamic(name):
+    return f"Hello {escape(name)}"
+
+@app.route("/json/<name>")
+def json(name):
+    return {
+        "data": {
+            "name": name
+        }
+    }
