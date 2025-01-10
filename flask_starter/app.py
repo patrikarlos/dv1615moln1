6609b2b8d749bc01c29616fbdca7bd979651709d
Patrik Arlos <Patrik.Arlos@bth.se>
2025-01-10 17:05:29 +0100

    Changed app, to print some networking info.

--- flask_starter/app.py
+++ flask_starter/app.py
@@ -1,9 +1,32 @@
-from flask import Flask
+from flask import Flask, request
 from markupsafe import escape
+from datetime import datetime
+import socket
 
 app = Flask(__name__)
 
+visits = 0 # Init gobal
+STYLE = " NATIVE(1) "
+
 @app.route("/")
+def default():
+    global visits # Grab visits from 'global' 
+    visits +=1 # Increment +11
+    now = datetime.now()
+    myNAME=socket.gethostname()
+    myHOST=request.headers.get('Host')
+    if request.environ.get('HTTP_X_FORWARDED_FOR') is None:
+        theirADDR=request.remote_addr
+        theirPORT=request.environ.get('REMOTE_PORT')
+
+    else:
+        theirADDR=request.environ['HTTP_X_FORWARDED_FOR']
+        theirADDR=request.environ['PORT']  ## Does not work if behind proxy... 
+    
+    timestamp = now.strftime("%Y-%m-%d %H:%M:%S")
+    return timestamp + STYLE + myNAME + "(" + myHOST + ")"+ " " + str(visits) +  ". From " + theirADDR + ":" +  str(theirPORT) + "!\n" # Return string with more info
+
+@app.route("/hello")
 def hello_world():
     return "Hello World!"
 
